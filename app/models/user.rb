013b2d5c8a7d99b3d463149704afdffed429a22d
benedetta von palombini <benedetta.palombini@t-online.de>
2022-11-22 12:09:11 +0100

    adding belongs_to to the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :bookings
-  has_many :experiences
+  has_many :bookings, dependent: :destroy
+  has_many :experiences, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
