eccee83c9281bff3df725029b48e13fdbd97f0eb
Madeleine McDonald <mpmstarling@gmail.com>
2022-11-25 14:30:33 +0100

    fixed columns

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,7 +5,6 @@ class Booking < ApplicationRecord
   STATUS = ["pending", "accepted", "declined"]
   validates :date, presence: true
   validates :status, inclusion: { in: STATUS }
-
-  belongs_to :magician, class_name: "User"
-  belongs_to :customer, class_name: "User"
+  # belongs_to :magician, class_name: "User"
+  # belongs_to :customer, class_name: "User"
 end
